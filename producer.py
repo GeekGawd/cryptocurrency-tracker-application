574b7c91afea285bea79e05828e5b409730fa0a1
Suyash Singh <suyashsingh.stem@gmail.com>
2024-01-04 22:30:03 +0530

    Fix: Delete Redundant scripts

--- producer.py
+++
@@ -1,9 +0,0 @@
-from kafka import KafkaProducer
-from datetime import datetime
-import json
-producer = KafkaProducer(
-    bootstrap_servers=['localhost:9093'],
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
-)
-for i in range(1000):
-    producer.send('posts', {'author': 'suyash', 'content': 'Kafka is cool!', 'created_at': datetime.now().isoformat()})
